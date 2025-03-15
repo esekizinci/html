0275480c8469e57344ec3d7fa86758c8d479e978
Eser 8. <bootcamp@sekizinci.com>
2025-03-15 20:45:15 +0100

    minor changes

--- basit_form/web/action.php
+++ basit_form/web/action.php
@@ -3,17 +3,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $ad = htmlspecialchars($_POST["ad"]);
     $soyad = htmlspecialchars($_POST["soyad"]);
     $posta = htmlspecialchars($_POST["posta"]);
-    #$kardes = htmlspecialchars($_POST["kardes"]);
-    #$cinsiyet = htmlspecialchars($_POST["cinsiyet"]);
-    #$sigorta = htmlspecialchars($_POST["sigorta"]);
-    #$ok = htmlspecialchars($_POST["ok"]);
 
     $veri = ["ad" => $ad, "soyad" => $soyad, "posta" => $posta];
 
-    $dosya = "formdb.json";
+    $dosya = __DIR__ . "/formdb.json";  // TAM DOSYA YOLU KULLANILDI
 
     if (file_exists($dosya)) {
         $mevcut_veriler = json_decode(file_get_contents($dosya), true);
+        if (!is_array($mevcut_veriler)) {
+            $mevcut_veriler = [];
+        }
         $mevcut_veriler[] = $veri;
     } else {
         $mevcut_veriler = [$veri];
@@ -21,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     file_put_contents($dosya, json_encode($mevcut_veriler, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 
-    echo "Form basariyla kaydedildi!";
+    echo "Form başarıyla kaydedildi!";
 
     echo '<br><br><a href="index.html" style="
     display: inline-block;
